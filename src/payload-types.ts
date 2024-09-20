f0979b120d662177ea9638690d178b40eb2b07d2
Edwin Mong'are <37651243+edwinmongare@users.noreply.github.com>
2024-09-20 18:38:46 +0300

    Update payload-types.ts

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -115,6 +115,6 @@ export interface PayloadMigration {
 }
 
 
-declare module 'payload' {
-  export interface GeneratedTypes extends Config {}
-}
+//declare module 'payload' {
+  //export interface GeneratedTypes extends Config {}
+//}
