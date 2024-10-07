efa7e07ef16888baaa1cf7fd650d3ed1e5482b3d
troyferstol <troyferstol100@gmail.com>
2024-10-07 04:16:07 +0300

    lol

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,9 @@ import { cookies } from "next/headers";
 import UserAccountNav from "./UserAccountNav";
 import MobileNav from "./MobileNav";
 
+// Ensure the cookies and user data are fetched synchronously
 const Navbar = async () => {
+  // Fetch cookies in the appropriate context
   const nextCookies = cookies();
   const { user } = await getServerSideUser(nextCookies);
 
@@ -28,82 +30,55 @@ const Navbar = async () => {
 
               <div className="ml-auto flex items-center">
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                  {user ? null : (
-                    <Link
-                      href="/sign-in"
-                      className={buttonVariants({
-                        variant: "ghost",
-                      })}
-                    >
-                      Sign in
-                    </Link>
-                  )}
-
-                  {user ? null : (
-                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
-                  )}
-                  {user && (
-                    <Link
-                      href="/verify-certificate"
-                      className={buttonVariants({
-                        variant: "ghost",
-                      })}
-                    >
-                      Verify Certificate
-                    </Link>
-                  )}
-                  {user && user.role === "superadmin" && (
+                  {!user ? (
                     <>
                       <Link
-                        href="/analytics"
-                        className={buttonVariants({
-                          variant: "ghost",
-                        })}
+                        href="/sign-in"
+                        className={buttonVariants({ variant: "ghost" })}
                       >
-                        Dashboard
+                        Sign in
                       </Link>
                       <span
                         className="h-6 w-px bg-gray-200"
                         aria-hidden="true"
                       />
+                      <Link
+                        href="/sign-up"
+                        className={buttonVariants({ variant: "ghost" })}
+                      >
+                        Create account
+                      </Link>
                     </>
-                  )}
-
-                  {user && (
-                    <Link
-                      href="/view-orders"
-                      className={buttonVariants({
-                        variant: "ghost",
-                      })}
-                    >
-                      My Orders
-                    </Link>
-                  )}
-
-                  {user ? (
-                    <UserAccountNav user={user} />
                   ) : (
-                    <Link
-                      href="/sign-up"
-                      className={buttonVariants({
-                        variant: "ghost",
-                      })}
-                    >
-                      Create account
-                    </Link>
-                  )}
-
-                  {user ? (
-                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
-                  ) : null}
-
-                  {user ? null : (
-                    <div className="flex lg:ml-6">
-                      <span
-                        className="h-6 w-px bg-gray-200"
-                        aria-hidden="true"
-                      />
-                    </div>
+                    <>
+                      <Link
+                        href="/verify-certificate"
+                        className={buttonVariants({ variant: "ghost" })}
+                      >
+                        Verify Certificate
+                      </Link>
+                      {user.role === "superadmin" && (
+                        <>
+                          <Link
+                            href="/analytics"
+                            className={buttonVariants({ variant: "ghost" })}
+                          >
+                            Dashboard
+                          </Link>
+                          <span
+                            className="h-6 w-px bg-gray-200"
+                            aria-hidden="true"
+                          />
+                        </>
+                      )}
+                      <Link
+                        href="/view-orders"
+                        className={buttonVariants({ variant: "ghost" })}
+                      >
+                        My Orders
+                      </Link>
+                      <UserAccountNav user={user} />
+                    </>
                   )}
                 </div>
               </div>
